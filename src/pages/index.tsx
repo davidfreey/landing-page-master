1aa2e5ed045a918b00a0e1c58930a4ab88655372
David Freitas <davidsonmfreitas@gmail.com>
2021-03-21 22:46:03 -0300

    install last netlify-cli

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,6 @@ import JsonSchema from 'components/JsonSchema'
 import client from 'graphql/client'
 import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
 import { LandingPageProps } from 'types/api'
-import Assets from './assets'
 
 const Index = ({
   logo,
